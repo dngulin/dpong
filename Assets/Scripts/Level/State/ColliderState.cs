abca556b45442cbff81bf7a14cd2fe9f5b493c90
Danil Gulin <dngulin@yandex.ru>
2019-11-16 13:04:19 +0300

    Small performance improvements in `ColliderState`

--- Assets/Scripts/Level/State/ColliderState.cs
+++ Assets/Scripts/Level/State/ColliderState.cs
@@ -9,25 +9,28 @@ namespace DPong.Level.State {
     public SnVector2 Position {
       get => _position;
       set {
+        if (_position == value)
+          return;
+
         _position = value;
         _transform = GetTransform(value);
       }
     }
 
-    public ColliderState(SnVector2 position) {
+    public ColliderState(in SnVector2 position) {
       _position = position;
       _transform = GetTransform(position);
     }
 
-    private static SnMatrix3 GetTransform(SnVector2 position) {
+    private static SnMatrix3 GetTransform(in SnVector2 position) {
       return Shape2D.GetTransform(new ShapePose2D {Position = position});
     }
 
-    public ShapeState2D ToRect(RectSize2D size) {
+    public ShapeState2D ToRect(in RectSize2D size) {
       return new ShapeState2D(ShapeType2D.Rect, new ShapeSize2D(size), _transform);
     }
 
-    public ShapeState2D ToCircle(CircleSize2D size) {
+    public ShapeState2D ToCircle(in CircleSize2D size) {
       return new ShapeState2D(ShapeType2D.Circle, new ShapeSize2D(size), _transform);
     }
   }
