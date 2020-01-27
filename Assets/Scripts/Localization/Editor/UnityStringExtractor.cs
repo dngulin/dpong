1fbf1e18da26389b9f824a71ab27fc93f9c82ad7
Danil Gulin <dngulin@yandex.ru>
2020-01-27 20:01:12 +0300

    Generate POT-file with prefab strings instead of pseudo-c source file
    PoEdit extractor config
    Extensions: `*.pot`
    Command: `‪xgettext --force-po -o %o %C %K %F`

--- Assets/Scripts/Localization/Editor/UnityStringExtractor.cs
+++ Assets/Scripts/Localization/Editor/UnityStringExtractor.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Text;
 using UnityEditor;
@@ -12,14 +11,10 @@ namespace DPong.Localization.Editor {
     [MenuItem(GenerateFileMenu)]
     private static void GetLocalizedText() {
       Debug.Log("Collecting strings from prefab...");
-      var outputPath = Path.Combine(Application.dataPath, "prefab-strings.gettext");
+      var outputPath = Path.Combine(Application.dataPath, "prefab-extracted-strings.pot");
 
       using (var fileStream = File.Create(outputPath))
       using (var textWriter = new StreamWriter(fileStream, new UTF8Encoding(false))) {
-        textWriter.WriteLine("// Auto-generated file! Do not edit!");
-        textWriter.WriteLine($"// Execute `{GenerateFileMenu}` to update contents");
-        textWriter.WriteLine($"// Generated at {DateTime.Now}\n");
-
         foreach (var guid in AssetDatabase.FindAssets("t:prefab")) {
           var path = AssetDatabase.GUIDToAssetPath(guid);
           var asset = AssetDatabase.LoadAssetAtPath<GameObject>(path);
@@ -27,24 +22,37 @@ namespace DPong.Localization.Editor {
 
           Debug.Log($"Processing prefab: {path}...");
           foreach (var loader in loaders) {
+            textWriter.WriteLine($"#: {path}/{GetObjectPath(loader.transform)}");
+
+            if (!string.IsNullOrEmpty(loader.Context))
+              textWriter.WriteLine($"msgctxt \"{Escape(loader.Context)}\"");
+
             var text = loader.GetComponent<Text>().text;
-            var escapedText = text.Replace("\"", "\\\"");
-
-            textWriter.Write("\n// ");
-            textWriter.WriteLine(path);
-
-            if (string.IsNullOrEmpty(loader.Context)) {
-              textWriter.WriteLine($"_(\"{escapedText}\");");
-            }
-            else {
-              var escapedContext = loader.Context.Replace("\"", "\\\"");
-              textWriter.WriteLine($"_p(\"{escapedContext}\", \"{escapedText}\");");
-            }
+            textWriter.WriteLine($"msgid \"{Escape(text)}\"");
+            textWriter.WriteLine("msgstr \"\"\n");
           }
         }
       }
 
       Debug.Log("Done!");
+    }
+
+    private static string GetObjectPath(Transform obj) {
+      var path = obj.name;
+
+      while (obj.parent != null) {
+        obj = obj.parent;
+        path = $"{obj.name}/{path}";
+      }
+
+      return path;
+    }
+
+    private static string Escape(string text) {
+      return text
+        .Replace("\\", "\\\\")
+        .Replace("\"", "\\\"")
+        .Replace("\n", "\\n");
     }
   }
 }
