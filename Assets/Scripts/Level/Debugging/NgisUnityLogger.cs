af617aeb7f2429fbf2296d000624740b5e2d0e09
Danil Gulin <dngulin@yandex.ru>
2020-12-15 21:44:46 +0300

    Update NGIS

--- Assets/Scripts/Level/Debugging/NgisUnityLogger.cs
+++ Assets/Scripts/Level/Debugging/NgisUnityLogger.cs
@@ -1,15 +1,29 @@
 using System;
+using NGIS.Logging;
 using UnityEngine;
-using ILogger = NGIS.Logging.ILogger;
 
 namespace DPong.Level.Debugging {
-  public class NgisUnityLogger : ILogger {
-    public void Info(string msg) => Debug.Log($"SESSION: {msg}");
+  public class NgisUnityLogger : IClientSessionLogger {
+    public void Connecting(string host, int port) => Debug.Log($"Connecting to {host}:{port}");
 
-    public void Warning(string msg) => Debug.LogWarning($"SESSION: {msg}");
+    public void Joining(string playerName, string game, ushort version) {
+      Debug.Log($"Joining as {playerName} ({game}:{version})");
+    }
 
-    public void Error(string msg) => Debug.LogError($"SESSION: {msg}");
+    public void Joined() => Debug.Log("Joined!");
+    public void GameStarted() => Debug.Log("Game started!");
+    public void GameFinished() => Debug.Log("Game finished!");
+    public void ConnectionLost() => Debug.LogError("Connection lost!");
+    public void SessionClosed() => Debug.Log("Session closed");
 
-    public void Exception(Exception e) => Debug.LogError($"SESSION: {e}");
+    public void FailedToProcessSession(Exception exception) {
+      Debug.LogError("Failed to process session!");
+      Debug.LogError(exception);
+    }
+
+    public void FailedToSendMessages(Exception exception) {
+      Debug.LogError("Failed to send messages!");
+      Debug.LogError(exception);
+    }
   }
 }
